a981c152980123809bb1be66d09ea5dafbbbd17f
10012 <nas7062@naver.com>
2025-08-14 22:22:00 +0900

    feat : 프로젝트 환경세팅

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold underline">Tailwind v4 + React 19 + pnpm OK</h1>
+    </div>
+  );
+}
+
+export default App;
