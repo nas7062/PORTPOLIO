6601dbcfe406053bf36da83767ae08e859676090
10012 <nas7062@naver.com>
2025-08-15 18:04:42 +0900

    feat : 프로젝트 내용 및 css 수정

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -4,7 +4,9 @@ export const projects = [
   {
     id: 1,
     title: '로드픽',
-    description: 'React와 TailwindCSS를 활용한 개인 포트폴리오 제작',
+    description: `사용자가 쉽고 체계적으로 여행 계획을 세울 수 있도록 돕는 기능을 중심으로, 
+      팀원 간의 협업을 통해 기획부터 디자인, 프론트엔드 구현까지 전 과정을 경험합니다.`,
+    stack: ['React', 'CSS Module', 'TANSTACK QUERY'],
     image: cardImage,
     github: 'https://github.com/username/portfolio',
     demo: 'https://portfolio.example.com',
@@ -12,7 +14,9 @@ export const projects = [
   {
     id: 2,
     title: 'U:MATE',
-    description: 'OpenWeather API를 활용한 실시간 날씨 정보 제공 앱',
+    description: `AI챗봇기반 통신사 요금제 추천 및 관리 서비스을 개발하는 것을 목표로 합니다.
+AI챗봇과 요금제 조회·비교·리뷰 기능을 통해 합리적인 선택을 돕고, 디지털 취약 계층(시니어)도 쉽게 이용할 수 있도록 접근성과 편의성을 강화하는 것이 목적입니다.`,
+    stack: ['React', 'Tailwind', 'TANSTACKQUERY', 'TypeScript'],
     image: cardImage,
     github: 'https://github.com/username/weather-app',
     demo: 'https://weather.example.com',
@@ -20,7 +24,12 @@ export const projects = [
   {
     id: 3,
     title: '지중해',
-    description: '할 일 관리와 필터링 기능이 있는 Todo 웹 애플리케이션',
+    description: `지중해는 통신사 멤버십 제휴처 안내 지도 서비스로
+혜택을 잘 모르는 사용자가 현재 위치를 기반으로 
+주변 제휴처와 혜택을 추천받고, 동네 사람들과 정보를 공유하며, 
+게이미피케이션 요소로 사용자의 접근성을 높이도록 설계한 서비스입니다.
+`,
+    stack: ['React', 'Tailwind', 'TANSTACKQUERY', 'TypeScript'],
     image: cardImage,
     github: 'https://github.com/username/todo-app',
     demo: 'https://todo.example.com',
@@ -35,7 +44,7 @@ export default function ProjectSection() {
         {projects.map((project) => (
           <div
             key={project.id}
-            className="flex h-72  w-[660px] text-center  border border-gray-500 rounded-xl overflow-hidden"
+            className="flex h-72  w-[660px] text-center  border border-gray-500   shadow-md hover:shadow-xl  hover:-translate-y-2 transform transition-all duration-300 rounded-xl overflow-hidden"
           >
             <img
               src={project.image}
@@ -44,12 +53,23 @@ export default function ProjectSection() {
               height={300}
               loading="lazy"
             />
-            <div className="flex flex-col flex-1 ">
-              <p>{project.title}</p>
-              <p>{project.description}</p>
-              <div className="flex justify-center gap-10 ">
-                <button>github</button>
-                <button>설명 보기</button>
+            <div className="flex flex-col flex-1 p-4 ">
+              <p className="text-2xl ">{project.title}</p>
+              <p className="px-2 text-gray-600">{project.description}</p>
+              <div className="py-2 ">
+                {project.stack?.map((st) => (
+                  <span className="ml-1 text-xs border text-white bg-sky-400 border-sky-400 rounded-lg px-2 py-1 ">
+                    {st}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-center gap-10 mt-auto ">
+                <button className="border border-gray-300 rounded-lg text-white px-2 py-1 bg-gray-800 hover:bg-gray-300 cursor-pointer hover:text-gray-800 ">
+                  Github
+                </button>
+                <button className="border border-gray-300 rounded-lg text-white px-2 py-1 bg-gray-800 hover:bg-gray-300 cursor-pointer hover:text-gray-800 ">
+                  설명 보기
+                </button>
               </div>
             </div>
           </div>
