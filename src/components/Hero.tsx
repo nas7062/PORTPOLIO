afd657115eff9c52b779ef5bf0d7669631806876
10012 <nas7062@naver.com>
2025-08-15 13:39:59 +0900

    style : hero 부분 애니메이션 수정

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,27 +8,47 @@ const container = {
     transition: { staggerChildren: 0.08, delayChildren: 0.1 },
   },
 };
-const item = {
-  hidden: { opacity: 0, y: 16 },
-  show: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+
+// 왼쪽에서 등장
+const fromLeft = {
+  hidden: { opacity: 0, x: -700 },
+  show: { opacity: 1, x: 0, transition: { duration: 2.1 } },
+};
+
+// 오른쪽에서 등장
+const fromRight = {
+  hidden: { opacity: 0, x: 700 },
+  show: { opacity: 1, x: 0, transition: { duration: 2.1 } },
 };
 
 export default function Hero() {
   const navigate = useNavigate();
   return (
     <motion.section
-      className="min-h-[80svh] flex flex-col items-center justify-center text-center"
+      className="min-h-[80svh] flex flex-col items-center justify-center text-center space-y-3"
       variants={container}
       initial="hidden"
       animate="show"
     >
-      <motion.h1 className="text-5xl font-bold" variants={item}>
-        Frontend Developer
-      </motion.h1>
-      <motion.p className="mt-4 text-lg text-neutral-600" variants={item}>
-        React • TypeScript • Tailwind
-      </motion.p>
-      <motion.div className="mt-8" variants={item}>
+      <motion.h2 className="text-8xl font-bold tracking-widest text-gray-700" variants={fromLeft}>
+        FRONTEND DEVELOPER
+      </motion.h2>
+      <motion.h2
+        className="mt-4 text-8xl font-bold  tracking-widest text-gray-700"
+        variants={fromRight}
+      >
+        USER EXPERIENCE
+      </motion.h2>
+      <motion.h2 className="text-8xl font-bold tracking-widest text-gray-700" variants={fromLeft}>
+        COMMUNICATION
+      </motion.h2>
+      <motion.h2
+        className="mt-4 text-8xl font-bold tracking-widest  text-gray-700"
+        variants={fromRight}
+      >
+        CREATIVITY
+      </motion.h2>
+      <motion.div className="mt-8" variants={container}>
         <button
           className="px-6 py-3 rounded-xl bg-black text-white cursor-pointer"
           onClick={() => navigate('/work')}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Paperlogy-8ExtraBold', 'sans-serif'],
+      },
+    },
+  },
+};
