02bb515499af4cc2be4827e6e4258bab0d120a8d
10012 <nas7062@naver.com>
2025-08-25 19:30:19 +0900

    style : 프로젝트 섹션 반응형 수정

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -9,29 +9,31 @@ export const projects = [
       팀원 간의 협업을 통해 기획부터 디자인, 프론트엔드 구현까지 전 과정을 경험합니다.`,
     stack: ['React', 'CSS Module', 'TANSTACK QUERY'],
     image: P1,
+    group: '프론트엔드 4명',
     github: 'https://github.com/nas7062/road-pick-fe',
     demo: 'https://roadpick.vercel.app/',
   },
   {
     id: 2,
     title: 'U:MATE',
-    description: `AI챗봇기반 통신사 요금제 추천 및 관리 서비스을 개발하는 것을 목표로 합니다.
-AI챗봇과 요금제 조회·비교·리뷰 기능을 통해 합리적인 선택을 돕고, 디지털 취약 계층(시니어)도 쉽게 이용할 수 있도록 접근성과 편의성을 강화하는 것이 목적입니다.`,
+    description: `
+AI챗봇과 요금제 조회·비교·리뷰 기능을 통해 합리적인 선택을 돕고, 디지털 취약 계층(시니어)도 쉽게 이용할 수 있도록 접근성과 편의성을 강화하는 것을 목표로 합니다.`,
     stack: ['React', 'Tailwind', 'TANSTACK QUERY', 'TypeScript'],
     image: P2,
+    group: '프론트엔드 5명',
     github: 'https://github.com/nas7062/U-Mate',
     demo: 'https://umate.vercel.app/',
   },
   {
     id: 3,
     title: '지중해',
-    description: `지중해는 통신사 멤버십 제휴처 안내 지도 서비스로
-혜택을 잘 모르는 사용자가 현재 위치를 기반으로 
-주변 제휴처와 혜택을 추천받고, 동네 사람들과 정보를 공유하며, 
-게이미피케이션 요소로 사용자의 접근성을 높이도록 설계한 서비스입니다.
+    description: `지중해는 통신사 멤버십 제휴처 지도 서비스로
+혜택을 잘 모르는 사용자가 위치를 기반으로 주변 제휴처와 혜택을 추천받고,정보를 공유하며, 
+게임 요소로 사용자의 접근성을 높이는걸 목표로 합니다.
 `,
     stack: ['React', 'Tailwind', 'TANSTACK QUERY', 'TypeScript'],
     image: P3,
+    group: '프론트엔드 4명 백엔드 3명',
     github: 'https://github.com/nas7062/URECA-5CEAN',
     demo: 'https://jijonnghae.site/',
   },
@@ -47,33 +49,38 @@ export default function ProjectSection() {
             key={project.id}
             className="flex h-80  w-full text-center   border border-gray-200   shadow-md hover:shadow-xl  hover:-translate-y-2 transform transition-all duration-300 rounded-xl overflow-hidden"
           >
-            <img
-              src={project.image}
-              alt="프로젝트 이미지"
-              width={280}
-              height={280}
-              loading="lazy"
-              className="rounded-xl w-60 h-64 self-center ml-2"
-            />
+            <div className="flex flex-col justify-center">
+              <img
+                src={project.image}
+                alt="프로젝트 이미지"
+                width={280}
+                height={280}
+                loading="lazy"
+                className="rounded-xl w-40 h-40 sm:w-60 sm:h-64 self-center ml-2"
+              />
+              <div className="text-xs sm:text-base mt-4">팀원:{project.group}</div>
+            </div>
+
             <div className="flex flex-col flex-1 p-4 ">
-              <p className="text-2xl ">{project.title}</p>
-              <p className="px-2 text-gray-600">{project.description}</p>
-              <div className="py-1 flex flex-wrap gap-1 mt-2 ">
+              <p className="text-md sm:text-2xl  ">{project.title}</p>
+              <p className="sm:px-2 text-gray-600 text-xs sm:text-base">{project.description}</p>
+              <div className="py-1 flex flex-wrap gap-1 sm:mt-2 ">
                 {project.stack?.map((st, idx) => (
                   <span
                     key={idx}
-                    className="ml-1 text-xs border text-white bg-sky-400 border-sky-400 rounded-lg px-2 py-1 "
+                    className="ml-1 text-[8px] sm:text-xs border text-white bg-sky-400 border-sky-400 rounded-lg px-2 py-1 "
                   >
                     {st}
                   </span>
                 ))}
               </div>
-              <div className="flex justify-center gap-10 mt-auto ">
+
+              <div className="flex sm:justify-center gap-1 sm:gap-10 mt-auto flex-col sm:flex-row  ">
                 <a
                   href={project.github}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="border border-gray-300 rounded-lg text-white px-2 py-1 bg-gray-800 hover:bg-gray-300 cursor-pointer hover:text-gray-800"
+                  className="border border-gray-300 rounded-lg text-xs sm:text-base text-white px-2 py-1 bg-gray-800 hover:bg-gray-300 cursor-pointer hover:text-gray-800"
                 >
                   Github
                 </a>
@@ -81,7 +88,7 @@ export default function ProjectSection() {
                   href={project.demo}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="border border-gray-300 rounded-lg text-white px-2 py-1 bg-gray-800 hover:bg-gray-300 cursor-pointer hover:text-gray-800"
+                  className="border border-gray-300 rounded-lg text-xs sm:text-base text-white px-2 py-1 bg-gray-800 hover:bg-gray-300 cursor-pointer hover:text-gray-800"
                 >
                   View Site
                 </a>
